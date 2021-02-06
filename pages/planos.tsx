8a4fcf695f23fe75e3807da4393bf59998db7a7b
adrianoyuji <adrianoyuji@gmail.com>
2021-02-06 00:00:52 -0400

    added missing pages but WIP

---
+++ pages/planos.tsx
@@ -0,0 +1,11 @@
+import Layout from "components/ui/Layout/index";
+
+const PlanosIndex = () => (
+  <Layout title="Planos | Le Green Investimentos">
+    <div className="h-screen">
+      <p className="text-white text-6xl"> 🚧 Página em construção 🚧</p>
+    </div>
+  </Layout>
+);
+
+export default PlanosIndex;
